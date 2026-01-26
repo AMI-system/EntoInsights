30e3754eb7f080ab8a13068e5c57ef9e910b20b7
gls21 <GraSki@ceh.ac.uk>
2026-01-26 12:21:18 +0000

    adding function for wrangling moth results into standardised format

--- R/wrangle_to_standardised_format.R
+++ R/wrangle_to_standardised_format.R
@@ -10,7 +10,7 @@
 #' @param filepath_colname A string specifying the name of the column containing filepaths
 #' @param sitename_colname A string specifying the name of the column containing site names
 #' @param latitude_colname A string specifying the name of the column containing latitudes
-#' @param longitude_colname A string specifying the name of the column containing longitude
+#' @param longitude_colname A string specifying the name of the column containing longitudes
 #' @param topspeciespred_colname A string specifying the name of the column containing top species predictions
 #' @param speciesconf_colname A string specifying the name of the column containing species confidence scores
 #'
@@ -26,6 +26,9 @@ wrangle_results <- function(results_file, taxa, filepath_colname,
     stop("Can only wrangle moth results currently. Need to adapt for bird and bat")
   }
 
+  # # For testing
+  # results_file <- "C:/Users/graski/OneDrive - UKCEH/part-time_PhD/AgZero+/analysis/agzero_ami_2024_data/east_hendred/combined_results.csv"
+
   # Check file exists
   if (!file.exists(results_file)) {
     stop("File does not exist: ", results_file)
@@ -34,6 +37,14 @@ wrangle_results <- function(results_file, taxa, filepath_colname,
   results <- read.csv(results_file)
 
   ### Checks and wrangling
+
+  # # For testing
+  # filepath_colname <- "image_path"
+  # sitename_colname <- "deployment_name"
+  # latitude_colname <- "latitude"
+  # longitude_colname <- "longitude"
+  # topspeciespred_colname <- "top_1_species"
+  # speciesconf_colname <- "top_1_confidence"
 
   # Check all columns necessary are present:
   required_cols <- c(
@@ -62,16 +73,63 @@ wrangle_results <- function(results_file, taxa, filepath_colname,
   names(results)[names(results) == speciesconf_colname] <- "species_confidence"
 
   # 2.	Get date column - either update date colname OR create new column containing date
+  ## For moths, the date and time the image was taken is in image_datetime column
+  ## Make image_datetime column a datetime object
+  results$image_datetime <- as.POSIXct(results$image_datetime, format = "%Y-%m-%d %H:%M:%S", tz="GMT")
+  # Extract the date component
+  results$date <- as.Date(results$image_datetime)
+
   # 3.	Get time column - create new column from datetime column
-  # 4.	Get sampling_day_or_night column - create new column from datetime column
+  # Extract time component
+  results$time <- format(results$image_datetime, format = "%H:%M:%S")
 
-  # 	Join lookup tables (site → lat/lon) if needed
+  # 4.	Get sampling_day_or_night (image_datetime) column - potentially need to create new column from datetime column
+  ## For moths, this is now in the outputted results file under the column "recording_session"
+  ## Make image_datetime column a datetime object
+  results$recording_session <- as.POSIXct(results$recording_session, format = "%Y-%m-%d", tz="GMT")
 
-  # 	Correct variable types e.g., Coerces confidence to numeric
+  # Join lookup tables (site → lat/lon) if needed - not needed for moths as lat/lon already in outputted results
 
-  # 	Flags missing lat/lon/site issues early
+  # Correct variable types e.g., Coerces confidence to numeric - all variable types seem appropriate for moths
 
-  # 	Flag nights where too few or too many images – indication something went wrong on those nights.
+  # Flags missing lat/lon/site issues early
+  # Return warning if any rows have empty latitude, longitude, or site_name columns
+  missing_info <- results[
+    is.na(results$site_name) | results$site_name == "" |
+      is.na(results$latitude)  |
+      is.na(results$longitude),
+  ]
+
+  if (nrow(missing_info) > 0) {
+    warning(
+      sprintf(
+        "Found %d rows with missing site_name and/or latitude/longitude",
+        nrow(missing_info)
+      )
+    )
+  }
+
+  # Flag nights where too few or too many images – indication something went wrong on those nights.
+  ## For moths, meant to be image every minute between sunrise and sunset. Average 10 hours night between April and Oct, so expect ~600 images a night.
+  # Flag if below 100 images (based on unique filepaths per recording session) or above 1000
+  recording_session_counts <- results %>%
+    dplyr::group_by(site_name, recording_session) %>%
+    dplyr::summarise(
+      n_images = dplyr::n_distinct(filepath),
+      .groups = "drop"
+    )
+
+  problem_recording_session <- recording_session_counts %>%
+    dplyr::filter(n_images < 100 | n_images > 1000)
+
+  if (nrow(problem_recording_session) > 0) {
+    warning(
+      sprintf(
+        "Detected %d nights with anomalous image counts (<100 or >1000).",
+        nrow(problem_recording_session)
+      )
+    )
+  }
 
   return(results)
 
