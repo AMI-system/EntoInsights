5b1a4ecbf97164eb8967a240fdc445ff2767d634
gls21 <GraSki@ceh.ac.uk>
2026-03-19 14:50:14 +0000

    adding example workflow using package

---
+++ R/EntoInsights-package.R
@@ -0,0 +1,4 @@
+#' EntoInsights
+#'
+#' Provides a range of functions to support users of the Automated Monitoring of Insects (AMI) and LepiSense systems. Its functionality broadly assists users who wish to analyse classification datasets, as well as partners reviewing and retrieving their uploaded image and acoustic datasets.
+"_PACKAGE"
